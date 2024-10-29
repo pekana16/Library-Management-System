e25dd790bbdef0c8cc8e60322554b4cbc5ec5c33
pekana16 <pekic.milos11@gmail.com>
2024-10-29 22:02:24 +0100

    borrowingservice fix

--- src/main/java/com/library/management/borrowingservice/Service/BorrowingService.java
+++ src/main/java/com/library/management/borrowingservice/Service/BorrowingService.java
@@ -15,24 +15,13 @@ public class BorrowingService {
     private final RestTemplate restTemplate;
 
 
-    /* had issues with getting bookservice.url to work properly as
-     intended -> will have to come back to "@Value" inject as some point
-      in the future to make the URL configurable
-     -> for now i chose to hardcode the URL - look at line 34
-    */
-
-    /*
-    @Value("${bookservice.url}")
-    private String bookServiceUrl;
-    */
-
     public BorrowingService(RestTemplateBuilder builder) {
         this.restTemplate = builder.build();
     }
 
     public ResponseEntity<Book> borrowBook(Long id) {
         String bookServiceUrl = "http://localhost:8081";
-        String url = bookServiceUrl + "/books" + id + "/borrow";
+        String url = bookServiceUrl + "/books/" + id;
         return restTemplate.postForEntity(url, null, Book.class);
     }
 
